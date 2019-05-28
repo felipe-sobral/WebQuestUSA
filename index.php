93f4174cdfcb804a38d384c8bdb49dbc821d9a40
Felipe Sobral <krepper@outlook.com>
2019-05-28 18:01:49 -0300

    2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         
         <nav>
             <div class="nav-wrapper menu-topo">
-                <a href="#!" class="brand-logo center">EUA</a>
+                <a href="#!" class="brand-logo center">USA</a>
                 <a href="#" data-activates="menu" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="side-nav fixed tamanho-menu" id="menu">
                     <li>
@@ -23,23 +23,60 @@
                             <div class="background">
                                 <img src="imagens/estatua_liberdade.jpg" width="200px">
                             </div>
-                            <a href="#!user"><img class="circle" src="http://archives.materializecss.com/0.100.2/images/yuna.jpg"></a>
-                            <a href="#!name"><span class="white-text name">Colombozinho</span></a>
-                            <a href="#!email"><span class="white-text email">Explorador</span></a>
+                            <a href="?p=introducao"><img class="circle" src="imagens/perfil.jpg"></a>
+                            <a href="?p=introducao"><span class="white-text name">Colombozinho</span></a>
+                            <a href="?p=introducao"><span class="white-text email">Explorador</span></a>
                         </div>
                     </li>
-                    <li><a href="#">Início</a></li>
-                    <li><a href="#">Tarefas</a></li>
-                    <li><a href="#">Processo</a></li>
-                    <li><a href="#">Recursos</a></li>
-                    <li><a href="#">Avaliação</a></li>
-                    <li><a href="#">Sobre</a></li>
+                    <li><a href="?p=inicio">Início</a></li>
+                    <li><a href="?p=introducao">Introdução</a></li>
+                    <li><a href="?p=tarefa">Tarefa</a></li>
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header" style="padding-left: 32px">Processos</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#!">Etapa 1</a></li>
+                                    <li><a href="#!">Etapa 2</a></li>
+                                </ul>
+                                <hr>
+                            </div>
+                        </li>
+                    </ul>
+                    <li><a href="?p=recursos">Recursos</a></li>
+                    <li><a href="?p=avaliacao">Avaliação</a></li>
+                    <li><a href="?p=conclusao">Conclusão</a></li>
+                    <li><a href="?p=sobre">Sobre</a></li>
                 </ul>
             </div>
         </nav>
         
 
-        <h1>{CONTEÚDO}</h1>
+        <?php
+
+            $pagina = isset($_GET["p"]) ? $_GET["p"] : "unknow";
+            
+            switch($pagina){
+                case "inicio":
+                    include_once "paginas/inicio.html";
+                    break;
+
+                case "introducao":
+                    include_once "paginas/introducao.html";
+                    break;
+
+                case "tarefa":
+                    include_once "paginas/tarefa.html";
+                    break;
+
+                default:
+                    include_once "paginas/inicio.html";
+            }
+        ?>
+
+        <div class="container rodape">
+            <p>Atualizado em 05/2019 - versão 1.0 - Trabalho acadêmico da disciplina de Didática da Computação na UFPR - Setor Palotina.</p>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
